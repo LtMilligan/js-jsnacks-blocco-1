ce03b2b9941f4e0b37bf0d76cd78e0b2b8980c8c
LtMilligan <gioele.miscia@gmail.com>
2024-07-01 19:37:24 +0200

    [UPD] SNACK 2 completed

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -4,5 +4,11 @@ const numeriUser = []
 // Creo un ciclo FOR con 6 iterazioni con il prompt per far insierire i dati all'user
 for (let i = 1; i <= 6; i++) {
     let numberz = prompt('Inserisci un numero')
-    
+    // Calcolo tramite IF se il valore inserito è pari o dispari
+    // In caso di dato dispari, faccio il push all'interno dell'arrey.
+    if (numberz % 2 !== 0) {
+        numeriUser.push(numberz)
+    }
 }
+
+console.log(numeriUser)
