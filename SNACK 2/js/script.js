4d94a35a5624fec6a53871e8285e9b2e4f360b99
LtMilligan <gioele.miscia@gmail.com>
2024-07-01 19:25:34 +0200

    [UPD] SNACK 2 Adding prompt in cycle FOR

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -1,3 +1,8 @@
 // Creo un array vuoto.
 const numeriUser = []
 
+// Creo un ciclo FOR con 6 iterazioni con il prompt per far insierire i dati all'user
+for (let i = 1; i <= 6; i++) {
+    let numberz = prompt('Inserisci un numero')
+    
+}
