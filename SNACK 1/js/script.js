4ce2b0719e8f0c3d9b0fc610eb2504e29c5fedbf
LtMilligan <gioele.miscia@gmail.com>
2024-07-01 19:06:39 +0200

    [UPD] Completed check user input

--- SNACK 1/js/script.js
+++ SNACK 1/js/script.js
@@ -1,2 +1,23 @@
 // Creare un array con i nomi degli invitati.
-let invitati = ['Pippo', 'Paperino', 'Qui', 'Quo', 'Archimede']
+const invitati = ['Pippo', 'Paperino', 'Qui', 'Quo', 'Archimede']
+
+// Confronto il dato dell'user con i nomi nell'array.
+
+const btnConferma = document.getElementById('confermaInvito')
+
+let validUser = false
+console.log(validUser)
+
+btnConferma.addEventListener('click', function () {
+    let userName = document.getElementById('invitato').value
+    console.log(userName)
+    console.log(validUser)
+    for (let i = 0; i < invitati.length; i++) {
+        if (invitati[i] == userName) {
+            validUser = true
+        }
+    }
+    console.log(validUser)
+    
+
+})
